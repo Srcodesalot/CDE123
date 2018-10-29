07be803416f61ee171f20d4b120545df962a1ca1
Aidan <aidan.gutierrez@pop.belmont.edu>
2018-10-28 21:30:03 -0500

    Initial Commit

---
+++ CDEto123Tests/CDEto123Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CDEto123Tests.swift
+//  CDEto123Tests
+//
+//  Created by Aidan Gutierrez on 10/28/18.
+//  Copyright © 2018 Aidan Gutierrez. All rights reserved.
+//
+
+import XCTest
+@testable import CDEto123
+
+class CDEto123Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
